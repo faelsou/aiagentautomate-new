01e0e315d0c159458de1442f274ce9581d99f58e
faelsou <rafadsouz@gmail.com>
2025-02-27 23:16:12 -0300

    Revert "restart"
    This reverts commit c8dcce44943fe68bb002bf0555943f322773af86.

--- src/api/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-// src/api/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
