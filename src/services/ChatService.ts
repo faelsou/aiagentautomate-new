152057de79eb415a4235fe6bf8f65dbfaaeaa6db
faelsou <rafadsouz@gmail.com>
2025-02-28 02:15:28 -0300

    ajuste pipeline e minikube

--- src/services/ChatService.ts
+++ src/services/ChatService.ts
@@ -43,7 +43,7 @@ export class ChatService {
     }
   }
 
-  onMessage(callback: (message: any) => void) {
+  onMessage(callback: (message: string) => void) {
     this.socket.on('message', callback);
   }
 
